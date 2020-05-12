4a8e5c6d350221d2d3883bd2fc28d22986269b04
simonwa7 <simonwa7@gmail.com>
2020-05-12 10:39:04 -0400

    Updating from orquestra namespace to zquantum

--- src/setup.py
+++ src/setup.py
@@ -2,13 +2,13 @@ import setuptools
 import os
 
 setuptools.setup(
-    name="orquestra-qcbm",
+    name="z-quantum-qcbm",
     version="0.1.0",
     author="Zapata Computing, Inc.",
     author_email="info@zapatacomputing.com",
     description="QCBM package for Orquestra.",
     url="https://github.com/zapatacomputing/z-quantum-qcbm ",
-    packages=setuptools.find_namespace_packages(include=['orquestra.*']),
+    packages=setuptools.find_namespace_packages(include=['zquantum.*']),
     package_dir={'' : 'python'},
     classifiers=(
         "Programming Language :: Python :: 3",
