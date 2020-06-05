4900df330a0eacb9f3b7f678154be602ac047ebf
Max Radin <radin.max@gmail.com>
2020-06-05 18:42:05 -0400

    Updating BAS tests

--- src/python/zquantum/qcbm/target_test.py
+++ src/python/zquantum/qcbm/target_test.py
@@ -11,7 +11,7 @@ class TestTarget(unittest.TestCase):
         self.methods = [ "zigzag" ]
         self.grids = [ [1, 1], [1, 2], [2, 1], [2, 2], [3, 2], [2, 3], [3, 3]]
         self.number_of_patterns = [2, 4, 4, 6, 10, 10, 14 ]
-        self.percentages = [ 1., 0.75, 0.5, 0.25, 0.1 ]
+        self.fractions = [ 1., 0.75, 0.5, 0.25, 0.1 ]
 
     def test_get_num_bars_and_stripes_patterns(self):
         for i, grid in enumerate(self.grids):
@@ -24,13 +24,13 @@ class TestTarget(unittest.TestCase):
     def test_get_bars_and_stripes_target_distribution_all_methods(self):
         for method in self.methods:
             for i, grid in enumerate(self.grids):
-                for percentage in self.percentages:
+                for fraction in self.fractions:
                     # When
-                    bars_and_stripes_distribution = get_bars_and_stripes_target_distribution(grid[0], grid[1],  percentage=percentage, method=method)
+                    bars_and_stripes_distribution = get_bars_and_stripes_target_distribution(grid[0], grid[1],  fraction=fraction, method=method)
                     
                     # Then
                     self.assertIsInstance(bars_and_stripes_distribution, BitstringDistribution)
-                    expected_num_patters = max(int(self.number_of_patterns[i]*percentage), 1)
+                    expected_num_patters = max(int(self.number_of_patterns[i]*fraction), 1)
                     self.assertEqual(len(bars_and_stripes_distribution.distribution_dict.keys()), expected_num_patters)
 
     def test_get_bars_and_stripes_target_distribution_unsupported_method(self):
@@ -40,7 +40,7 @@ class TestTarget(unittest.TestCase):
         ncols = 2
 
         # When/Then
-        self.assertRaises(RuntimeError, lambda: get_bars_and_stripes_target_distribution(nrows, ncols, percentage=1., method=method))
+        self.assertRaises(RuntimeError, lambda: get_bars_and_stripes_target_distribution(nrows, ncols, fraction=1., method=method))
 
     def test_bars_and_stripes_zigzag_2_by_2(self):
         # Given
