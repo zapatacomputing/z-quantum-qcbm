bfe3cafa8414214c7c774df7cefe983d396efa00
Marta Mauri <marta.mauri14@gmail.com>
2020-07-20 15:01:15 +0200

    adding tests for mmd distance measure

--- src/python/zquantum/qcbm/cost_function_test.py
+++ src/python/zquantum/qcbm/cost_function_test.py
@@ -4,9 +4,15 @@ from .ansatz import QCBMAnsatz
 from zquantum.core.bitstring_distribution import (
     BitstringDistribution,
     compute_clipped_negative_log_likelihood,
+    compute_mmd,
 )
 from zquantum.core.utils import create_object, ValueEstimate
 import numpy as np
+
+param_list = [
+    (compute_clipped_negative_log_likelihood, {"epsilon": 1e-6}),
+    (compute_mmd, {"sigma": 1}),
+]
 
 
 class TestQCBMCostFunction(unittest.TestCase):
@@ -15,8 +21,6 @@ class TestQCBMCostFunction(unittest.TestCase):
         number_of_qubits = 4
         topology = "all"
         self.ansatz = QCBMAnsatz(number_of_layers, number_of_qubits, topology)
-
-        self.distance_measure = compute_clipped_negative_log_likelihood
         self.target_bitstring_distribution = BitstringDistribution(
             {
                 "0000": 1.0,
@@ -47,54 +51,61 @@ class TestQCBMCostFunction(unittest.TestCase):
         )
 
     def test_init(self):
-        # Given
-        epsilon = 1e-6
+        for distance_meas, distance_measure_params in param_list:
+            with self.subTest():
+                # Given
+                self.distance_measure = distance_meas
+                distance_measure_parameters = distance_measure_params
 
-        # When
-        cost_function = QCBMCostFunction(
-            self.ansatz,
-            self.backend,
-            self.distance_measure,
-            self.target_bitstring_distribution,
-            epsilon,
-        )
+            # When
+            cost_function = QCBMCostFunction(
+                self.ansatz,
+                self.backend,
+                self.distance_measure,
+                distance_measure_parameters,
+                self.target_bitstring_distribution,
+            )
 
-        # Then
-        self.assertEqual(cost_function.ansatz, self.ansatz)
-        self.assertEqual(cost_function.backend, self.backend)
-        self.assertEqual(cost_function.distance_measure, self.distance_measure)
-        self.assertEqual(
-            cost_function.target_bitstring_distribution,
-            self.target_bitstring_distribution,
-        )
-        self.assertEqual(cost_function.epsilon, epsilon)
-        self.assertEqual(cost_function.gradient_type, "finite_difference")
-        self.assertEqual(cost_function.save_evaluation_history, True)
-        self.assertEqual(cost_function.evaluations_history, [])
+            # Then
+            self.assertEqual(cost_function.ansatz, self.ansatz)
+            self.assertEqual(cost_function.backend, self.backend)
+            self.assertEqual(cost_function.distance_measure, self.distance_measure)
+            self.assertEqual(
+                cost_function.target_bitstring_distribution,
+                self.target_bitstring_distribution,
+            )
+            self.assertEqual(
+                cost_function.distance_measure_parameters, distance_measure_parameters
+            )
+            self.assertEqual(cost_function.gradient_type, "finite_difference")
+            self.assertEqual(cost_function.save_evaluation_history, True)
+            self.assertEqual(cost_function.evaluations_history, [])
 
     def test_evaluate(self):
-        # Given
-        epsilon = 1e-6
+        for distance_meas, distance_measure_params in param_list:
+            with self.subTest():
+                # Given
+                self.distance_measure = distance_meas
+                distance_measure_parameters = distance_measure_params
 
-        cost_function = QCBMCostFunction(
-            self.ansatz,
-            self.backend,
-            self.distance_measure,
-            self.target_bitstring_distribution,
-            epsilon,
-        )
+                cost_function = QCBMCostFunction(
+                    self.ansatz,
+                    self.backend,
+                    self.distance_measure,
+                    distance_measure_parameters,
+                    self.target_bitstring_distribution,
+                )
 
-        params = np.array([0, 0, 0, 0])
+                params = np.array([0, 0, 0, 0])
 
-        # When
-        value_estimate = cost_function.evaluate(params)
-        history = cost_function.evaluations_history
+                # When
+                value_estimate = cost_function.evaluate(params)
+                history = cost_function.evaluations_history
 
-        # Then
-        self.assertEqual(type(value_estimate), ValueEstimate)
-        self.assertEqual(type(value_estimate.value), float)
-        self.assertIn("bitstring_distribution", history[0].keys())
-        self.assertEqual(dict, type(history[0]["bitstring_distribution"]))
-        self.assertIn("value", history[0].keys())
-        self.assertIn("params", history[0].keys())
-
+                # Then
+                self.assertEqual(type(value_estimate), ValueEstimate)
+                assert isinstance(value_estimate.value, (np.floating, float))
+                self.assertIn("bitstring_distribution", history[0].keys())
+                self.assertEqual(dict, type(history[0]["bitstring_distribution"]))
+                self.assertIn("value", history[0].keys())
+                self.assertIn("params", history[0].keys())
