1f9c775ea23021479816bb5502535a5ef14920aa
simonwa7 <simonwa7@gmail.com>
2021-01-07 17:24:59 -0500

    Fixing syntax issues

--- steps/generate_target_distribution.py
+++ steps/generate_target_distribution.py
@@ -2,7 +2,9 @@ from zquantum.qcbm.target import (
     get_bars_and_stripes_target_distribution as _get_bars_and_stripes_target_distribution,
 )
 from zquantum.core.bitstring_distribution import save_bitstring_distribution
-from zquantum.qcbm.target_thermal_states import  as _get_thermal_states_target_distribution
+from zquantum.qcbm.target_thermal_states import (
+    get_thermal_states_target_distribution as _get_thermal_states_target_distribution,
+)
 
 
 def get_bars_and_stripes_target_distribution(nrows, ncols, fraction, method):
@@ -14,8 +16,6 @@ def get_bars_and_stripes_target_distribution(nrows, ncols, fraction, method):
 
 
 def get_thermal_states_target_distribution(**kwargs):
-    distribution = _get_bars_and_stripes_target_distribution(
-        **kwargs
-    )
+    distribution = _get_thermal_states_target_distribution(**kwargs)
 
     save_bitstring_distribution(distribution, "distribution.json")
