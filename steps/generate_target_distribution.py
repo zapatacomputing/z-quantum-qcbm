c1fdb999af3a0eb96f25ad8bd5acd5b99be95740
kaitlinmgili <kaitlinmgili@gmail.com>
2021-01-08 18:29:35 +0000

    Added way to save to save thermal target states to json file

--- steps/generate_target_distribution.py
+++ steps/generate_target_distribution.py
@@ -1,7 +1,13 @@
 from zquantum.qcbm.target import get_bars_and_stripes_target_distribution as _get_bars_and_stripes_target_distribution
 from zquantum.core.bitstring_distribution import save_bitstring_distribution
+from zquantum.qcbm.target_thermal_states import get_thermal_states_target_distribution as _get_thermal_states_target_distribution
 
 def get_bars_and_stripes_target_distribution(nrows, ncols, fraction, method):
     distribution = _get_bars_and_stripes_target_distribution(nrows, ncols, fraction, method)
+
+    save_bitstring_distribution(distribution, "distribution.json")
+
+def get_thermal_states_target_distribution(n_spins, beta, n_body_interaction):
+    distribution = _get_thermal_states_target_distribution(n_spins, beta, n_body_interaction)
 
     save_bitstring_distribution(distribution, "distribution.json")
