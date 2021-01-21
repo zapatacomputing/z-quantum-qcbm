5c2d65616eac94e7c56f10ad28834cdc47ee9aa7
kaitlinmgili <kaitlinmgili@gmail.com>
2021-01-21 14:10:12 +0000

    changes because of target thermal data file changes

--- steps/generate_target_distribution.py
+++ steps/generate_target_distribution.py
@@ -1,7 +1,7 @@
 from zquantum.qcbm.target import get_bars_and_stripes_target_distribution as _get_bars_and_stripes_target_distribution
 from zquantum.core.bitstring_distribution import save_bitstring_distribution
 from zquantum.core.bitstring_distribution import save_bitstring_distribution_set
-from zquantum.qcbm.target_thermal_states import get_thermal_states_target_distribution as _get_thermal_states_target_distribution
+from zquantum.qcbm.target_thermal_states import get_target_Bitstring_Dist as _get_thermal_states_target_distribution
 from zquantum.core.bitstring_distribution import load_bitstring_distribution_set
 
 
@@ -11,14 +11,14 @@ def get_bars_and_stripes_target_distribution(nrows, ncols, fraction, method):
 
     save_bitstring_distribution(distribution, "distribution.json")
 
-def get_thermal_states_target_distribution(n_spins, beta, n_body_interaction):
-    distribution = _get_thermal_states_target_distribution(n_spins, beta, n_body_interaction)
+def get_thermal_states_target_distribution(n_spins, beta, seed_dist):
+    distribution = _get_thermal_states_target_distribution(n_spins, beta, seed_dist)
 
     save_bitstring_distribution(distribution, "distribution.json")
 
 
-def get_thermal_target_training_set(n_spins, beta, n_body_interaction, instances): 
-    training_data_set_distributions = [ _get_thermal_states_target_distribution(n_spins, beta, n_body_interaction) for _ in range(instances) ]
+def get_thermal_target_training_set(n_spins, beta, instances): 
+    training_data_set_distributions = [ _get_thermal_states_target_distribution(n_spins, beta, i) for i in range(instances) ]
     
     save_bitstring_distribution_set(training_data_set_distributions, "distribution_set.json" )
 
